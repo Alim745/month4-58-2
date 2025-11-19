ff5f2e337c561b50495d50757a24d496c066ba7f
Alim Maratov <alimmaratov11@gmail.com>
2025-11-19 17:47:48 +0600

    hw3

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Post(models.Model):
+    image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=300)
     content = models.TextField(max_length=1000, blank=True)
     rate = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- posts/views.py
+++ posts/views.py
@@ -12,3 +12,8 @@ def html_view(request):
 def posts_list_view(request):
     posts = Post.objects.all()
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts/post_detail.html', {'post': post})
